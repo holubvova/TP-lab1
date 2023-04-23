d20bd07ee7fda9c9b7b5d0db70ac914518b30dda
tsu-khan-yuk <62913085+tsu-khan-yuk@users.noreply.github.com>
2023-04-23 23:43:22 +0300

    Added new modification

--- sort_hoara.py
+++ sort_hoara.py
@@ -12,3 +12,14 @@ def quick_sort(arr):
                 right.append(arr[i])
         return quick_sort(left) + [pivot] + quick_sort(right)
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+
