c663df095e7031e6be62e6429f1644b6aace738d
vovka <vovkaevrey@ukr.net>
2023-04-23 23:47:45 +0300

    add cat

--- new.py
+++ new.py
@@ -1,15 +1,19 @@
 
-def bubble_sort(arr):
-    n = len(arr)
-    # Проходимо по масиву n-1 разів
-    for i in range(n-1):
-        # Проходимо по масиву з кроком n-i-1, оскільки після кожної ітерації
-        # найбільший елемент відсортовано та можна його ігнорувати в майбутньому
-        for j in range(0, n-i-1):
-            # Порівнюємо сусідні елементи
-            if arr[j] > arr[j+1]:
-                # Якщо перший елемент більше другого, міняємо їх місцями
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+from art import text2art
 
-print("Conflit was fixed.Thank you in andvance!")
+def print_cat():
+    cat = text2art(" /\_/\ \n( o   o )\n (  =^=  )\n  (") + ")\n"
+    print(cat)
+
+# Виклик функції для виведення зображення котика
+print_cat()
+
+
+"""
+ /\_/\
+( o   o )
+ (  =^=  )
+  (     )
+
+"""
 
