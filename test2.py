7c11926d99a3eb5267dd8c994a8bae458df60e1d
Volodymyr Holub <62032405+holubvova@users.noreply.github.com>
2023-06-08 14:51:42 +0300

    Update fix  test2.py

--- test2.py
+++ test2.py
@@ -1,33 +1,28 @@
-def calculate_rectangle_area(length, width):
-    area = length1 * width1
-    return area
-
-def calculate_square_area(side):
-    area = side * side
-    return area
-
+def calculate_area(length, width):
+    return length * width
 
 def is_even(num):
-    if num % 2 == 0:
-        return True
-    else:
-        return False
-
-def calculate_average_grade(grade1, grade2, grade3, grade4, grade5):
-    total = grade1 + grade2 + grade3 + grade4 + grade5
-    average = total / 5
-    return average
+    return num % 2 == 0
 
 
-def calculate_foo(x, y):
-    res = x * y
-    return res
+def calculate_average_grade(*grades):
+    return sum(grades) / len(grades)
 
 
-def find_duplicate(numbers):
-    duplicates = []
-    for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
-            if numbers[i] == numbers[j] and numbers[i] not in duplicates:
-                duplicates.append(numbers[i])
-    return duplicates
+def calculate_product(x, y):
+    return x * y
+
+
+
+def find_duplicates(numbers):
+    duplicates = set()
+    seen = set()
+
+    for num in numbers:
+        if num in seen:
+            duplicates.add(num)
+        else:
+            seen.add(num)
+
+    return list(duplicates)
+
